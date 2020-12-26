6e9870a5b00bdcbbd023f5ff49eb9df41a705859
Mathan <mathanthirunavukkarasu@gmail.com>
2020-12-26 17:11:44 +0530

    upto button event handler

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var userName = prompt("gimme your name:");
+// var userName = prompt("gimme your name:");
 
 // alert("The script works fine!! " + userName);
 
-var welcomeMessage = "This script works!! " + userName;
+// var welcomeMessage = "This script works!! " + userName;
 
-alert(welcomeMessage);
+// alert(welcomeMessage);
+
+
+var btnTranslate = document.querySelector("#btn-translate")
+
+btnTranslate.addEventListener("click", function clickHandler() {
+    console.log("clicked");
+})
