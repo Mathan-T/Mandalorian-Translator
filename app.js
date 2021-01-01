d5d88bfed484e263943160b0045267a80b336237
Mathan <mathanthirunavukkarasu@gmail.com>
2021-01-01 18:01:15 +0530

    Replaced mock server URL with original server URL

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate")
 var textInput = document.querySelector("#txt-input")
 var textOutput = document.querySelector("#output")
 
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("Error occured", error);
+    alert("This translation app is rate limited to 5 per hour....please try after sometime!!")
+}
 
 function clickHandler() {
-    textOutput.innerText = "groottttttttt " + textInput.value;
+    var inputText = textInput.value;
+
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
