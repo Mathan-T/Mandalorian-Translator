19bad9df6f07fa90bb176f86f8e040dc085dad33
Mathan <mathanthirunavukkarasu@gmail.com>
2021-01-02 18:43:10 +0530

    added styles & bg-image

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var buttonTranslate = document.querySelector("#btn-translate")
 var inputText = document.querySelector("#txt-input")
 var outputText = document.querySelector("#output")
 
-var serverURL = "https://api.funtranslations.com/translate/sith.json"
+var serverURL = "	https://api.funtranslations.com/translate/mandalorian.json"
 
 
 function getTranslationURL(text) {
