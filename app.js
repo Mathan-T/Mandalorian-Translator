d188161e8f9771656a38eeda8d7a18ba8263a681
Mathan <mathanthirunavukkarasu@gmail.com>
2021-01-04 12:36:59 +0530

    Some corrections in app.js

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
-var buttonTranslate = document.querySelector("#btn-translate");
-var inputText = document.querySelector("#txt-input");
-var outputText = document.querySelector("#output");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/mandalorian.json"
 
-
 function getTranslationURL(text) {
-    return serverURL + "?" + "text= " + text
-};
-
+    return serverURL + "?" + "text=" + text
+}
 
 function errorHandler(error) {
     console.log("error occured", error);
-    alert("This translation app is free to use and rate limited to 5 times per hour....Please try after some time!!")
+    alert("This translation app is rate limited to 5 times per hour.Please try after some time!!)
 }
 
+// taking input
 function clickHandler() {
-    var textInput = inputText.value;
+    var inputText = txtInput.value;
 
-    fetch(getTranslationURL(textInput))
-    .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
-    .catch(errorHandler)
-};
+// calling server for processing
+fetch (getTranslationURL(inputText))
+.then (response => response.json())
+.then (json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+})
+.catch(errorHandler)
+}
 
-
-buttonTranslate.addEventListener("click", clickHandler)
-
-
-
-
-
-
-
-
-
+btnTranslate.addEventListener("click", clickHandler)
